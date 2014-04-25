714016ee622dd7a1d55016fe3d50aeed4db8a12c
spiritbear <indap.shiv@gmail.com>
2014-04-25 15:07:22 -0700

    Added a way to convert the response json into Hashie::Mashie

---
+++ lib/service_monster/response.rb
@@ -0,0 +1,19 @@
+module ServiceMonster
+  module Response
+    def self.create( response_hash )
+      data = response_hash.data.dup rescue response_hash
+      data.extend( self )
+      p "************"
+      return data
+      
+      # data.instance_exec do
+      #   @pagination = response_hash.pagination
+      #   @meta = response_hash.meta
+      # end
+      # data
+    end
+
+    attr_reader :pagination
+    attr_reader :meta
+  end
+end
